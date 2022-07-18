82e809fb2f5ac4604ca0252c47d7222cd6637c16
Harry J Robson <h+9@rryrobson.com>
2022-07-19 03:38:41 +0700

    Update solana-nft-airdrop.py

--- solana-nft-airdrop.py
+++ solana-nft-airdrop.py
@@ -10,10 +10,6 @@ token_file.close()
 #Count which address we are up to
 count = 0
 
-#Send a 0.1 Solana tip to creator. 
-#tip = ["solana", "transfer", "2tmH3Yj661FoRwfqFshw1jm4ocFdd2Q17aSAGU9JrMzK", "0.1", "--allow-unfunded-recipient"]
-#send_tip = subprocess.Popen(tip)
-
 #For the nth address we attempt to send token n
 with open(r'wallet_list.txt') as fp:
     for line in fp:
